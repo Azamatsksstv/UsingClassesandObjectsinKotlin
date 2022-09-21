6300ea9f106461fa15fad43ba7af143a411e5f01
Azamatskkstv <azambylbaev@gmail.com>
2022-09-21 17:28:15 +0500

    9.2.2

--- src/main/kotlin/example/myapp/decor/Decoration.kt
+++ src/main/kotlin/example/myapp/decor/Decoration.kt
@@ -1,8 +1,10 @@
 package example.myapp.decor
-
 import example.myapp.makeFish
 
 data class Decoration(val rocks: String) {
+}
+enum class Color(val rgb: Int) {
+    RED(0xFF0000), GREEN(0x00FF00), BLUE(0x0000FF);
 }
 fun makeDecorations() {
     val decoration1 = Decoration("granite")
@@ -17,7 +19,12 @@ fun makeDecorations() {
     println (decoration3.equals(decoration2))
 }
 
+enum class Direction(val degrees: Int) {
+    NORTH(0), SOUTH(180), EAST(90), WEST(270)
+}
 
-fun main () {
-    makeDecorations()
+fun main() {
+    println(Direction.EAST.name)
+    println(Direction.EAST.ordinal)
+    println(Direction.EAST.degrees)
 }
