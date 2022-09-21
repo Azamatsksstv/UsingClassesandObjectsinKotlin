440b718b734269cf124ccd2b7e24bd190f1d278f
Azamatskkstv <azambylbaev@gmail.com>
2022-09-21 16:44:27 +0500

    3.4

--- src/main/kotlin/example/myapp/Aquarium.kt
+++ src/main/kotlin/example/myapp/Aquarium.kt
@@ -1,9 +1,10 @@
 package example.myapp
 
-class Aquarium {
-    var width: Int = 20
-    var height: Int = 40
-    var length: Int = 100
+class Aquarium(length: Int = 100, width: Int = 20, height: Int = 40) {
+    var length: Int = length
+    var width: Int = width
+    var height: Int = height
+//    Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40)
     fun printSize() {
         println("Width: $width cm " +
                 "Length: $length cm " +
