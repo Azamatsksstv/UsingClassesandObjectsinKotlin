f6451eece62c920f0f9c7f7ae4410a07993b8a8b
Azamatskkstv <azambylbaev@gmail.com>
2022-09-21 16:52:51 +0500

    5.1.5

--- src/main/kotlin/example/myapp/Aquarium.kt
+++ src/main/kotlin/example/myapp/Aquarium.kt
@@ -1,22 +1,22 @@
 package example.myapp
 
-class Aquarium(length: Int = 100, width: Int = 20, height: Int = 40) {
-
-    var length: Int = length
-    var width: Int = width
-    var height: Int = height
-    var volume: Int
+open class Aquarium (open var length: Int = 100, open var width: Int = 20, open var height: Int = 40) {
+    open var volume: Int
         get() = width * height * length / 1000
         set(value) {
             height = (value * 1000) / (width * length)
         }
+    open val shape = "rectangle"
+    open var water: Double = 0.0
+        get() = volume * 0.9
+
     fun printSize() {
+        println(shape)
         println("Width: $width cm " +
                 "Length: $length cm " +
-                "Height: $height cm "
-        )
-        // 1 liter = 1000 cm^3
-        println("Volume: $volume liters")
+                "Height: $height cm ")
+        // 1 l = 1000 cm^3
+        println("Volume: $volume liters Water: $water liters (${water / volume * 100.0}% full)")
     }
 
     init {
